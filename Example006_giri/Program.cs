2d50c320d4e77075bdc85742cd68e276eaa379dd
Regina <gumerovars@gmail.com>
2023-01-07 01:13:15 +0500

    example 006 giri

---
+++ Example006_giri/Program.cs
@@ -0,0 +1,16 @@
+﻿int num1 = 1;
+int num2 = 2;
+int num3 = 6;
+int num4 = 8;
+int num5 = 4;
+
+int max = num1;
+
+if(num1 > max) max = num1;
+if(num2 > max) max = num2;
+if(num3 > max) max = num3;
+if(num4 > max) max = num4;
+if(num5 > max) max = num5;
+
+Console.Write("max = ");
+Console.WriteLine(max);
